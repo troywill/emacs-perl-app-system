71b0fcce09ca784331193ed543e9395bbd2bd7aa
Troy Will <troydwill@gmail.com>
2016-09-06 11:27:31 -0700

    modified:   bin/your-program.pl 	modified:   conf.conf 	modified:   emacs-perl-app-system.org

--- bin/your-program.pl
+++ bin/your-program.pl
@@ -1,29 +1,28 @@
 #!/usr/bin/env perl
-
 use warnings;
 use strict;
-use Getopt::Long;
-
-use FindBin qw($Bin);
-use File::Basename;
-use lib "$Bin/../lib";
-# require "foscam-perllib.pm";
-
-# read-config-chunk ??
-use AppConfig;
-
-# create a new AppConfig object
-my $config = AppConfig->new;
-
-# define a new variable
-# $config->define('LOCAL_DATABASE=s');
-$config->define("VAR1=s");
-
-# read configuration file
-
-print "$Bin <==\n";
-$config->file("$Bin/../conf.conf");
-
-my $VAR1 = $config->get("VAR1");
-
-print "VAR1 = $VAR1\n";
+###### path-block ############
+use FindBin qw($Bin);        #
+use File::Basename;          #
+use lib "$Bin/../lib";       #
+require "foscam-perllib.pm"; #
+##############################
+################configuration block #########
+use AppConfig;                              #
+                                            #
+# create a new AppConfig object             #
+my $config = AppConfig->new;                #
+                                            #
+# define a new variable                     #
+# $config->define('LOCAL_DATABASE=s');      #
+$config->define("VAR1=s");                  #
+                                            #
+# read configuration file                   #
+                                            #
+print "$Bin <==\n";                         #
+$config->file("$Bin/../<<.conf");           #
+                                            #
+my $VAR1 = $config->get("VAR1");            #
+                                            #
+print "VAR1 = $VAR1\n";                     #
+#############################################
